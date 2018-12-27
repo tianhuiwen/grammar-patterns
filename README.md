093aff916db116f411716828e3758507018df7f9
tianhuiwen <tianhuiwensir@163.com>
2018-12-27 21:10:23 +0800

    :sparkles: Add READNE.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# grammar-patterns
-Java 常用的设计模式总结，Java 8 语法的新特性，基本的算法
+## :roller_coaster: grammar-patterns
+Java 语言的进阶学习，包含 Java 8 语法的新特性，常用的设计模式，基本的算法
--- src/main/java/com/thw/top/patterns/design/strategy/demo/FruitPrice.java
+++ src/main/java/com/thw/top/patterns/design/strategy/demo/FruitPrice.java
@@ -21,7 +21,6 @@ public class FruitPrice {
 
     /**
      * @description 输出水果价格
-     * @date 2018年1月14日 下午3:12:26
      */
     public void printFruitPrice() {
         fruit.price();
